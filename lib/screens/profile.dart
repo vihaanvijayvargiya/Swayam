a233ff824aee0f63986733b95e47512e8f4bd73c
Vihaan Vijayvargiya <vihaanvijayvargiya@gmail.com>
2024-05-20 17:12:30 +0530

    update

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -24,7 +24,7 @@ class ProfilePage extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         flexibleSpace: Padding(
-          padding: const EdgeInsets.only(top: 20, bottom: 10), // Adjust padding here
+          padding: const EdgeInsets.only(top: 20, bottom: 10),
           child: Center(
             child: Text(
               'Profile',
@@ -54,48 +54,98 @@ class ProfilePage extends StatelessWidget {
 
           Map<String, dynamic> userData = snapshot.data!;
           print('User data: $userData');
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (user.photoURL != null)
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: NetworkImage(user.photoURL!),
-                  ),
-                SizedBox(height: 16),
-                Text(
-                  'Name: ${userData['username']}',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Email: ${user.email}',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Weight: ${userData['weight']}',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Height: ${userData['height']}',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Age: ${userData['age']}',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ],
-            ),
-          );
+          return ProfileScreen(userData: userData, userEmail: user.email ?? '');
         },
       )
           : Center(
         child: Text('No user is signed in'),
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  final Map<String, dynamic> userData;
+  final String userEmail;
+
+  const ProfileScreen({
+    required this.userData,
+    required this.userEmail,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            if (userData.containsKey('photoURL') && userData['photoURL'] != null)
+              CircleAvatar(
+                radius: 70,
+                backgroundImage: NetworkImage(userData['photoURL']),
+              ),
+            const SizedBox(height: 20),
+            _ProfileItem(title: 'Name', subtitle: userData['username'], iconData: Icons.person),
+            const SizedBox(height: 10),
+            _ProfileItem(title: 'Email', subtitle: userEmail, iconData: Icons.mail),
+            const SizedBox(height: 10),
+            _ProfileItem(title: 'Weight', subtitle: '${userData['weight'] ?? 'N/A'}', iconData: Icons.fitness_center),
+            const SizedBox(height: 10),
+            _ProfileItem(title: 'Height', subtitle: '${userData['height'] ?? 'N/A'}', iconData: Icons.height),
+            const SizedBox(height: 10),
+            _ProfileItem(title: 'Age', subtitle: '${userData['age'] ?? 'N/A'}', iconData: Icons.calendar_today),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(15),
+                ),
+                child: const Text('Edit Profile'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ProfileItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData iconData;
+
+  const _ProfileItem({
+    required this.title,
+    required this.subtitle,
+    required this.iconData,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 5),
+            color: Colors.teal.withOpacity(.2),
+            spreadRadius: 2,
+            blurRadius: 10,
+          )
+        ],
+      ),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        leading: Icon(iconData),
+        trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400),
+        tileColor: Colors.white,
       ),
     );
   }
